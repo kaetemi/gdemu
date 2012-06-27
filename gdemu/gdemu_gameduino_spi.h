c76bb4f3b083bcad32fe2143903f4e421613a393
kaetemi <kaetemi@localhost>
2012-06-27 15:37:49 +0200

    Added: Support for new VIDEO_MODE register

--- gdemu/gdemu_gameduino_spi.h
+++ gdemu/gdemu_gameduino_spi.h
@@ -7,20 +7,20 @@
  * \author Jan Boon (Kaetemi)
  */
 
-/* 
+/*
  * Copyright (C) 2011  Jan Boon (Kaetemi)
- * 
+ *
  * This file is part of GAMEDUINO EMULATOR.
  * GAMEDUINO EMULATOR is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * GAMEDUINO EMULATOR is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GAMEDUINO EMULATOR; see the file COPYING.  If not, see
  * <http://www.gnu.org/licenses/>.
@@ -72,10 +72,14 @@ public:
 
 	static uint8_t *getRam();
 
+	static inline double getRefreshTimeSeconds() { return s_RefreshTime; };
+
 private:
 	GameduinoSPIClass(const GameduinoSPIClass &);
 	GameduinoSPIClass &operator=(const GameduinoSPIClass &);
-	
+
+	static double s_RefreshTime;
+
 }; /* class GameduinoSPIClass */
 
 extern GameduinoSPIClass GameduinoSPI;
