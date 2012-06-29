d5c8559fb0105aec39c3d27f9587bb54f1e4533d
kaetemi <kaetemi@localhost>
2012-06-29 17:31:37 +0200

    Added: Missing prog space stuff

--- gdemu/inttypes.h
+++ gdemu/inttypes.h
@@ -7,20 +7,20 @@
  * \author Jan Boon (Kaetemi)
  */
 
-/* 
+/*
  * Copyright (C) 2011  by authors
- * 
+ *
  * This file is part of GAMEDUINO EMULATOR.
  * GAMEDUINO EMULATOR is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * GAMEDUINO EMULATOR is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GAMEDUINO EMULATOR; see the file COPYING.  If not, see
  * <http://www.gnu.org/licenses/>.
@@ -48,6 +48,7 @@ typedef signed __int64 int64_t;
 #endif
 typedef uint8_t prog_uint8_t;
 typedef uint16_t prog_uint16_t;
+typedef uint32_t prog_uint32_t;
 
 #endif /* #ifndef INTTYPES_H */
 
