a7b93cd354ab4942064cd903ec5692dd81853252
Jan Boon <kaetemi@users.noreply.github.com>
2012-06-29 17:40:58 +0200

    Added: Setting for guaranteed j1 raster chasing cycles

--- gdemu/gdemu_emulator.h
+++ gdemu/gdemu_emulator.h
@@ -7,20 +7,20 @@
  * \author Jan Boon (Kaetemi)
  */
 
-/* 
+/*
  * Copyright (C) 2011  Jan Boon (Kaetemi)
- * 
+ *
  * This file is part of GAMEDUINO EMULATOR.
  * GAMEDUINO EMULATOR is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * GAMEDUINO EMULATOR is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GAMEDUINO EMULATOR; see the file COPYING.  If not, see
  * <http://www.gnu.org/licenses/>.
@@ -43,20 +43,20 @@ namespace GDEMU {
 enum EmulatorFlags
 {
 	// enables the keyboard to be used as input
-	EmulatorEnableKeyboard = 0x01, 
+	EmulatorEnableKeyboard = 0x01,
 	// enables audio
-	EmulatorEnableAudio = 0x02, 
+	EmulatorEnableAudio = 0x02,
 	// enables j1
-	EmulatorEnableJ1 = 0x04, 
+	EmulatorEnableJ1 = 0x04,
 
 	// causes J1 to run on the main graphics thread
-	EmulatorSyncJ1GraphicsYLine = 0x10, 	
+	EmulatorSyncJ1GraphicsYLine = 0x10,
 	// causes J1 to run on the audio thread
-	EmulatorSyncJ1AudioSample = 0x20, 
+	EmulatorSyncJ1AudioSample = 0x20,
 	// combination of the two flags above, causes audio to sync with the graphics thread
-	
+
 	// links the FREQTICK register to audio sample playback, otherwise by system timer
-	EmulatorFreqTickByAudioSample = 0x40, 
+	EmulatorFreqTickByAudioSample = 0x40,
 };
 
 /**
@@ -71,11 +71,12 @@ public:
 	EmulatorClass() { }
 
 	static void run(void (*setup)(), void (*loop)(), int flags);
-	
+	static void setJ1RasterChasingCycles(int insnLimit = 256);
+
 private:
 	EmulatorClass(const EmulatorClass &);
 	EmulatorClass &operator=(const EmulatorClass &);
-	
+
 }; /* class EmulatorClass */
 
 extern EmulatorClass Emulator;
--- manicminer/main.cpp
+++ manicminer/main.cpp
@@ -7,6 +7,7 @@ void loop();
 // int __stdcall WinMain(void *, void *, void *, int)
 int main(int, char* [])
 {
+	GDEMU::Emulator.setJ1RasterChasingCycles(2048);
 	GDEMU::Emulator.run(setup, loop,
 		GDEMU::EmulatorEnableKeyboard | GDEMU::EmulatorEnableJ1 | GDEMU::EmulatorEnableAudio
 		);
